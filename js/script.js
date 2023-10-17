64e2f13392a493395a648e8b24f5074f29d283a6
a_yahya <a_yahya@rasana.ir>
2023-10-17 19:31:20 +0330

    cards and animation

--- js/script.js
+++ js/script.js
@@ -1,45 +1,59 @@
 
-$(document).ready(function() {
-    // $.ajax({
-    //     url: 'data.json',
-    //     dataType: 'json',
-    //     success: function(data) {
-    //         console.log(data)
-    //         // const moviesContainer = $('#movies-container')
-    //         // data.forEach(movie => {
-    //         //     const movieCard = `
-    //         //     <div class="movie-card">
-    //         //         <img class="movie-poster" src="${movie.poster}" alt="${movie.title_en}">
-    //         //     </div>
-    //         //     `
-            
-    //         //     moviesContainer.append(movieCard)
-    //         // });
-    //     },
-    //     error: function(xhr, status, error) {
-    //         console.log('Error:', error);
-    //     }
-    // })
-    
-    // $.getJSON('data.json', function(data) {
-    //     console.log(data)
-    // });
+$(document).ready(function () {
 
-    fetch('data.json')
-  .then(response => response.json())
-  .then(data => {
-    console.log(data)
-    const moviesContainer = $('#movies-container')
-    data.forEach(movie => {
+  fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+
+      const moviesContainer = $('#movies-container')
+
+      data.forEach((movie, index) => {
         const movieCard = `
-            <div class="movie-card">
-                <img class="movie-poster" src="${movie.poster}" alt="${movie.title_en}">
-            </div>`
-            console.log(movieCard)
-            moviesContainer.append(movieCard)
-        });
-})
-  .catch(error => {
-    console.error(error);
+            <div class="movie-card-container">
+              <a target='blank' href='${movie.link}'>
+                <img class="movie-poster hide" src="${movie.poster}" alt="${movie.title_en}">
+              </a>
+
+              <img class="movie-vhs show" src="${movie.vhs}" alt="${movie.title_en}">
+
+              <span class="title">${movie.title_fa}</span>
+              <span class="year">${movie.year}</span>
+            </div>
+            
+            `
+        moviesContainer.append(movieCard)
+      });
+    })
+    .catch(error => {
+      console.error(error);
+    });
+
+
+  $('#movies-container').on('click', '.movie-vhs', function () {
+
+    const shownPoster = $(this).parent().find('.movie-poster')
+
+    // add .clicked to the card container when user clicks on card
+    $('.movie-card-container').removeClass('clicked')
+    $(this).closest('.movie-card-container').addClass('clicked')
+
+    // hide this movie-vhs and show others
+    $(this).toggleClass('show hide')
+
+    // hide all the other posters if shown
+    const posterElems = $('.movie-poster').toArray()
+    posterElems.forEach((item, i) => {
+      if ($(item).hasClass('show')) {
+        $(item).toggleClass('show hide')
+        $(item).parent().parent().find('.movie-vhs').toggleClass('hide show')
+      }
+    })
+
+    // // show this poster
+    shownPoster.toggleClass('hide show')
+
+    // shownPoster.on('click', function () {
+
+    // })
   });
 })
